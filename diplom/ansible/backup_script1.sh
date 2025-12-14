8a8a11f0370c8abcb60585719877d1f6424a8bad
shchukin34-source <shchukin34@yandex.ru>
2025-12-14 12:43:50 +0200

    Update backup_script1.sh

--- diplom/ansible/backup_script1.sh
+++ diplom/ansible/backup_script1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Скрипт для копирования директории MediaWiki и передачи на другой сервер
+# Скрипт для создания dump MediaWiki и передачи на другой сервер
 sudo -u postgres pg_dump my_wiki > mywikidump.sql
-scp mywikidump.sql ubuntu@192.168.10.6:/home/ubuntu/
+scp -o StrictHostKeyChecking=no mywikidump.sql ubuntu@192.168.10.6:/home/ubuntu/
 rm -rf /home/ubuntu/mywikidump.sql
