b664b54a7f5e3d96c61cb905971fd9880c3f66c1
shchukin34-source <shchukin34@yandex.ru>
2025-12-14 12:43:28 +0200

    Update backup_script.sh

--- diplom/ansible/backup_script.sh
+++ diplom/ansible/backup_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Скрипт для копирования директории MediaWiki и передачи на другой сервер
 tar zcvhf wikidata.tgz /var/www/mediawiki
-scp -r /home/ubuntu/wikidata.tgz ubuntu@192.168.10.4:/home/ubuntu/
+scp -o StrictHostKeyChecking=no /home/ubuntu/wikidata.tgz ubuntu@192.168.10.4:/home/ubuntu/
 rm /home/ubuntu/wikidata.tgz
